b077f3effa569c2f44957f49123d6a85107aa5ad
Fredele20 <victorisholaoladele@gmail.com>
2023-08-08 09:44:35 +0100

    updated code to work for all test cases

--- practice-questions/codility/binary-gap/main.go
+++ practice-questions/codility/binary-gap/main.go
@@ -2,30 +2,39 @@ package main
 
 import "fmt"
 
-// This solution has only 60% correctness, will work on it.
+// This solution works perfect.
 func solution(N int) int {
 	// Implement your solution here
+	// This array is going to store binary digit
 	var arr []int
+	// Count holds the actual gap.
 	var count int
+	// Counter increments every time there is a zero
 	var counter int
+	// CurrentCount keep tracks of the current count
 	var currentCount int
 
-	for N != 0 {
+	// here we populate the array with the binary equivalent of the given number
+	for N > 0 {
 		arr = append([]int{N % 2}, arr...)
 		N /= 2
 	}
-
-	for i := 1; i < len(arr)-1; i++ {
+	
+	// Here, we loop through the slice arr
+	for i := 1; i < len(arr); i++ {
+		// Check if the value of the current index is 0 and increment counter
+		// also stores counter as the current count
 		if arr[i] == 0 {
 			counter++
 			currentCount = counter
-			if arr[len(arr)-1] == 0 {
-				currentCount = 0
-			}
-			if currentCount > count {
-				count = currentCount
-			}
 		}
+		// Checks if the current count is greater than the count and
+		// the current index value is 1, then stores currentCount to count
+		if currentCount > count && arr[i] == 1 {
+			count = currentCount
+		}
+		// Checks if the current index value is 1 and reset couter to zero
+		// This helps to start counting from the next zero when the current value is 1
 		if arr[i] == 1 {
 			counter = 0
 		}
@@ -35,7 +44,7 @@ func solution(N int) int {
 
 func main() {
 
-	n := solution(32)
+	n := solution(1376796946)
 	fmt.Println(n)
 
 }
