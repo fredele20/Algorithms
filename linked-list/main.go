9f801ef53193c78ddbe0593eb57b971892cc6882
Fredele20 <victorisholaoladele@gmail.com>
2023-08-15 08:39:50 +0100

    added simple linked-list implementation

---
+++ linked-list/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Node struct {
+	data int
+	next *Node
+}
+
+type LinkedList struct {
+	head   *Node
+	length int
+}
+
+func (l *LinkedList) prepend(n *Node) {
+	second := l.head
+	l.head = n
+	l.head.next = second
+	l.length++
+}
+
+func (l LinkedList) printListData() {
+	toPrint := l.head
+	for l.length != 0 {
+		fmt.Printf("%d ", toPrint.data)
+		toPrint = toPrint.next
+		l.length--
+	}
+	fmt.Printf("\n")
+}
+
+func (l *LinkedList) deleteWithValue(value int) {
+	if l.length == 0 {
+		return
+	}
+
+	if l.head.data == value {
+		l.head = l.head.next
+		l.length--
+		return
+	}
+
+	previousToDelete := l.head
+	for previousToDelete.next.data != value {
+		if previousToDelete.next.next == nil {
+			return
+		}
+		previousToDelete = previousToDelete.next
+	}
+	previousToDelete.next = previousToDelete.next.next
+	l.length--
+}
+
+func main() {
+	myList := LinkedList{}
+	node1 := &Node{data: 48}
+	node2 := &Node{data: 29}
+	node3 := &Node{data: 32}
+	node4 := &Node{data: 92}
+	node5 := &Node{data: 75}
+	node6 := &Node{data: 20}
+
+	myList.prepend(node1)
+	myList.prepend(node2)
+	myList.prepend(node3)
+	myList.prepend(node4)
+	myList.prepend(node5)
+	myList.prepend(node6)
+
+	myList.printListData()
+	myList.deleteWithValue(20)
+	myList.printListData()
+}
