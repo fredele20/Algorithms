50fae5f9d5fb9a7f1ca7834457810c563a1efe6c
Fredele20 <victorisholaoladele@gmail.com>
2023-11-02 15:02:47 +0100

    added length of last word algorithm

---
+++ practice-questions/leetcode/lengthOfLastWord/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+// A word is a maximal
+// substring
+//  consisting of non-space characters only.
+
+// Example 1:
+
+// Input: s = "Hello World"
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+// Example 2:
+
+// Input: s = "   fly me   to   the moon  "
+// Output: 4
+// Explanation: The last word is "moon" with length 4.
+// Example 3:
+
+// Input: s = "luffy is still joyboy"
+// Output: 6
+// Explanation: The last word is "joyboy" with length 6.
+
+// Constraints:
+
+// 1 <= s.length <= 104
+// s consists of only English letters and spaces ' '.
+// There will be at least one word in s.
+
+func lengthOfLastWord(s string) int {
+	trimmed := strings.TrimSpace(s)
+	words := strings.Split(trimmed, " ")
+	return len(words[len(words)-1])
+}
+
+func main() {
+	word := "   fly me   to   the moon  "
+
+	fmt.Println(lengthOfLastWord(word))
+}
--- practice-questions/leetcode/plusOne/main.go
+++ practice-questions/leetcode/plusOne/main.go
@@ -1,9 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-// You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the  
-// integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does 
+// You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the
+// integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does
 // not contain any leading 0's.
 
 // Increment the large integer by one and return the resulting array of digits.
@@ -38,22 +40,20 @@ import "fmt"
 
 func plusOne(digits []int) []int {
 
-  for i := len(digits) - 1; i >= 0; i-- {
-    if digits[i] != 9 {
-      digits[i]++
-      return digits
-    } else {
-      digits[i] = 0
-    }
-  }
-  digits = append([]int{1}, digits...)
-  return digits
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] != 9 {
+			digits[i]++
+			return digits
+		} else {
+			digits[i] = 0
+		}
+	}
+	digits = append([]int{1}, digits...)
+	return digits
 }
 
 func main() {
 	digits := []int{2, 9, 9}
 
-	output := plusOne(digits)
-
-	fmt.Println("Output: ", output)
+	fmt.Println("Output: ", plusOne(digits))
 }
